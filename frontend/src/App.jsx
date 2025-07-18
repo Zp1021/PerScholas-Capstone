b3f6ee978af481dc48adc824c0121cb3f4759421
Zp1021 <90364004+Zp1021@users.noreply.github.com>
2025-07-18 11:23:08 -0400

    Set up a redirect route that takes users back to the main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<MainPage />} />       
+        <Route path='/' element={<MainPage />} />
+        <Route path='*' element={<Navigate to='/'/>} />
       </Routes>
     </>
   )
