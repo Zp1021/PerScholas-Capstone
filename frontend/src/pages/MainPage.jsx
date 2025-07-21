746ce629e9ad6f8cd61e85d742969645e38e16cc
Zp1021 <90364004+Zp1021@users.noreply.github.com>
2025-07-20 21:18:56 -0400

    Added gif to assets and styled the main page

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -2,6 +2,7 @@ import { Form } from "react-router-dom";
 // import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 // import ScoreForm from "../components/ScoreForm";
+import logo from "../assets/gather-round.gif"
 
 export default function MainPage() {
 
@@ -11,12 +12,15 @@ export default function MainPage() {
             <h1>Welcome to the Establishment</h1>
             <NavBar />
             <p className="greeting">
-                Hello! 
-                <br/>
+                Hello!
+                <br />
                 I'm so happy you chose to stop by.
-                <br/>
+                <br />
                 While you're here why not check out the game room and relax for a bit.
             </p>
+            <div className="gif-container">
+                <img className="cats" src={logo} alt="cats gathered around a campfire" />
+            </div>
         </div>
     )
 }
