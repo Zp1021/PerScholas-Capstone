e3c077f1ffcd524d25ca10ec83eb6a5b24ccc694
Zp1021 <90364004+Zp1021@users.noreply.github.com>
2025-07-20 20:35:32 -0400

    Added game configuration

---
+++ frontend/game components/GameConfig.jsx
@@ -0,0 +1,44 @@
+import Start from "./Start";
+import Phaser from "phaser";
+import { useEffect } from "react";
+
+
+export default function Game() {
+    const config = {
+        type: Phaser.AUTO,
+        title: 'Overlord Rising',
+        description: '',
+        parent: 'game-container',
+        width: 800,
+        height: 600,
+        backgroundColor: '#000000',
+        pixelArt: false,
+        scene: [
+            Start
+        ],
+        scale: {
+            mode: Phaser.Scale.FIT,
+            autoCenter: Phaser.Scale.CENTER_BOTH
+        },
+        physics: {
+            default: 'arcade',
+            arcade: {
+                debug: false,
+                gravity: { y: 500 }
+            }
+        },
+    }
+
+    useEffect(() => {
+        const game = new Phaser.Game(config);
+
+        return () => {
+            game.destroy(true);
+        }
+    }, [])
+    return (
+        <div>
+            <div id="phaser-container" />
+        </div>
+    )
+}
