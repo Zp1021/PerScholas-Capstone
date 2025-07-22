5dd35f2dde111425981981d2a95551d3db45b899
Zp1021 <90364004+Zp1021@users.noreply.github.com>
2025-07-22 12:04:21 -0400

    Added cors middleware to the backend

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 
 import 'dotenv/config'
 
@@ -14,6 +15,7 @@ const port = process.env.PORT
 
 // Middleware
 app.use(express.json())
+app.use(cors())
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
